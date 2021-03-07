caa5f86935f8b75d2787b6e4d3a79415f47d8d88
gaojunfeng <490771426@qq.com>
2021-03-07 11:53:35 +0800

    添加路由

--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -9,59 +9,27 @@
         v-model:selectedKeys="selectedKeys"
       >
         <template v-for="itme of menuList" :key="itme.id">
-          <a-sub-menu :key="itme.id" v-if="itme.children && itme.children.length > 0">
+          <a-sub-menu
+            :key="itme.id"
+            v-if="itme.children && itme.children.length > 0"
+          >
             <template #title>
               <span>
                 <component v-bind:is="'MailOutlined'" />
                 <span>{{ itme.name }}</span>
               </span>
             </template>
-            <a-menu-item v-for="childrenItme of itme.children" :key="childrenItme.id">{{ childrenItme.name }}</a-menu-item>
+            <a-menu-item
+              v-for="childrenItme of itme.children"
+              :key="childrenItme.id"
+              >{{ childrenItme.name }}</a-menu-item
+            >
           </a-sub-menu>
           <a-menu-item v-else :key="itme.id">
             <component v-bind:is="'PieChartOutlined'" />
             <span>{{ itme.name }}</span>
           </a-menu-item>
         </template>
-
-        <!-- <a-menu-item key="1">
-          <PieChartOutlined />
-          <span>Option 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <DesktopOutlined />
-          <span>Option 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <InboxOutlined />
-          <span>Option 3</span>
-        </a-menu-item>
-        <a-sub-menu key="sub1">
-          <template #title>
-            <span>
-              <MailOutlined />
-              <span>Navigation One</span>
-            </span>
-          </template>
-          <a-menu-item key="5">Option 5</a-menu-item>
-          <a-menu-item key="6">Option 6</a-menu-item>
-          <a-menu-item key="7">Option 7</a-menu-item>
-          <a-menu-item key="8">Option 8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span>
-              <AppstoreOutlined />
-              <span>Navigation Two</span>
-            </span>
-          </template>
-          <a-menu-item key="9">Option 9</a-menu-item>
-          <a-menu-item key="10">Option 10</a-menu-item>
-          <a-sub-menu key="sub3" title="Submenu">
-            <a-menu-item key="11">Option 11</a-menu-item>
-            <a-menu-item key="12">Option 12</a-menu-item>
-          </a-sub-menu>
-        </a-sub-menu>-->
       </a-menu>
     </a-layout-sider>
     <a-layout>
@@ -84,33 +52,84 @@
   </a-layout>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, toRefs, watch } from "vue";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  PieChartOutlined,
-  MailOutlined,
-  DesktopOutlined,
-  InboxOutlined,
-  AppstoreOutlined,
-} from "@ant-design/icons-vue";
+  defineComponent,
+  reactive,
+  toRefs,
+  watch,
+  PropType,
+  getCurrentInstance,
+} from "vue";
+import * as icons from "@ant-design/icons-vue";
+
+interface IMenuListItemProp {
+  id: never;
+  children: any[];
+  name: string;
+  path: string;
+}
 
 export default defineComponent({
   name: "canna-layout",
   props: {
     menuList: {
-      type: Array,
+      type: Array as PropType<IMenuListItemProp[]>,
       // required: true,
       default: [],
     },
   },
-  setup() {
+  // watch: {
+  //   $route(to, from) {
+  //     sessionStorage.setItem('to.path', to.path);
+  //     console.log(to.path);
+  //   }
+  // },
+  setup(props, context) {
+    // 获取路由信息
+    const instance: any = getCurrentInstance();
+    const proxy: any = instance.proxy;
+    // console.log(proxy.$root.$route);
+    const location: string = proxy.$root.$router.options.history.location;
+    // console.log(location);
+
     const state = reactive({
+      menuList: props.menuList,
       collapsed: false,
-      selectedKeys: ["1"],
-      openKeys: ["sub1"],
-      preOpenKeys: ["sub1"],
+      selectedKeys: [],
+      openKeys: [],
+      preOpenKeys: [],
     });
+
+    // 设置菜单
+    watch(
+      () => props.menuList,
+      (newValue, oldValue): void => {
+        state.menuList = newValue;
+        if (newValue.length > 0) {
+          // 高亮当前访问的路径的菜单
+          function selectMenu(
+            arr: IMenuListItemProp[],
+            parentItem?: IMenuListItemProp
+          ): void {
+            for (const item of arr) {
+              if (item.path === location) {
+                // 设置高亮的节点
+                state.selectedKeys = [item.id];
+                if (parentItem) {
+                  // 设置打开的节点
+                  state.openKeys = [parentItem.id];
+                  state.preOpenKeys = [parentItem.id];
+                }
+              } else {
+                selectMenu(item.children, item);
+              }
+            }
+          }
+          selectMenu(newValue);
+          // state.selectedKeys = [newValue[0].id]; // 默认选中，后面改成当前路径
+        }
+      }
+    );
 
     watch(
       () => state.openKeys,
@@ -118,24 +137,20 @@ export default defineComponent({
         state.preOpenKeys = oldVal;
       }
     );
-    const toggleCollapsed = () => {
-      state.collapsed = !state.collapsed;
-      state.openKeys = state.collapsed ? [] : state.preOpenKeys;
-    };
+
+    // window.addEventListener("pushState", function (e) {
+    //   console.log("change pushState");
+    // });
+    // window.addEventListener("replaceState", function (e) {
+    //   console.log("change replaceState");
+    // });
 
     return {
       ...toRefs(state),
-      toggleCollapsed,
     };
   },
   components: {
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    PieChartOutlined,
-    MailOutlined,
-    DesktopOutlined,
-    InboxOutlined,
-    AppstoreOutlined,
+    ...icons,
   },
 });
 </script>
